c8f2f39d9808aa854bd1499968a46de140f2e40d
Nikola Madic <>
2020-04-12 21:55:15 +0200

    Dodate metode u biblioteka interface: -sacuvajUJSONFajl -ucitajIzJSONFajla

--- mvnbiblioteka2020/src/main/java/biblioteka/interfejs/BibliotekaInterfejs.java
+++ mvnbiblioteka2020/src/main/java/biblioteka/interfejs/BibliotekaInterfejs.java
@@ -5,17 +5,18 @@ import java.util.List;
 import biblioteka.Autor;
 import biblioteka.Knjiga;
 
-
-
 public interface BibliotekaInterfejs {
 
-	public void dodajKnjigu(Knjiga knjiga);
-	
-	public void obrisiKnjigu(Knjiga knjiga);
-	
-	public List<Knjiga> vratiSveKnjige();
-	
-	public List<Knjiga> pronadjiKnjigu(Autor autor,
-			String isbn, String naslov, String izdavac);
-	
+    public void dodajKnjigu(Knjiga knjiga);
+
+    public void obrisiKnjigu(Knjiga knjiga);
+
+    public List<Knjiga> vratiSveKnjige();
+
+    public List<Knjiga> pronadjiKnjigu(Autor autor,
+            String isbn, String naslov, String izdavac);
+
+    public void sacuvajUJSONFajl(String path);
+
+    public void ucitajIzFajla(String path);
 }
